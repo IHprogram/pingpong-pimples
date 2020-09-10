458f518b29f516dea7edc79333491805ab375c5b
IHprogram <ihm20202020@gmail.com>
2020-09-10 19:55:32 +0900

    Reviewモデルのマイグレーションファイルの記述
    記述を追加した後、マイグレートを実行した。

--- db/migrate/20200910102343_create_reviews.rb
+++ db/migrate/20200910102343_create_reviews.rb
@@ -1,7 +1,17 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-
+      t.string      :name,            null: false
+      t.integer     :manufacture_id,  null: false
+      t.integer     :type_id,         null: false
+      t.integer     :hardness,        null: false
+      t.integer     :spin_id,         null: false
+      t.integer     :speed_id,        null: false
+      t.integer     :price,           null: false
+      t.integer     :level_id,        null: false
+      t.integer     :evaluation_id,   null: false
+      t.text        :content,         null: false
+      t.references  :user,            null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_021620) do
+ActiveRecord::Schema.define(version: 2020_09_10_102343) do
+
+  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "manufacture_id", null: false
+    t.integer "type_id", null: false
+    t.integer "hardness", null: false
+    t.integer "spin_id", null: false
+    t.integer "speed_id", null: false
+    t.integer "price", null: false
+    t.integer "level_id", null: false
+    t.integer "evaluation_id", null: false
+    t.text "content", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_09_09_021620) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "users"
 end
