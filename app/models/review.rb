c387c749af0a66e2a34cc45c48ef20ef97d674c0
IHprogram <ihm20202020@gmail.com>
2020-09-10 22:47:04 +0900

    ActiveHashに関するアソシエーションの設定

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,12 @@ class Review < ApplicationRecord
   has_many :likes
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :manufacture
+  belongs_to_active_hash :type
+  belongs_to_active_hash :spin
+  belongs_to_active_hash :speed
+  belongs_to_active_hash :level
+  belongs_to_active_hash :evaluation
 end
