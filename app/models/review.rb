742ec9aa995a94a46c24bb9a2f03208143d32d93
IHprogram <ihm20202020@gmail.com>
2020-09-10 23:00:11 +0900

    バリデーションの設定

--- app/models/review.rb
+++ app/models/review.rb
@@ -11,4 +11,20 @@ class Review < ApplicationRecord
   belongs_to_active_hash :speed
   belongs_to_active_hash :level
   belongs_to_active_hash :evaluation
+
+  HALF_WIDTH_NUMBER_REGEX = /\A[0-9]+\z/
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :manufacture_id, numericality: { other_than: 1, message: 'Select' }
+    validates :type_id,        numericality: { other_than: 1, message: 'Select' }
+    validates :hardness,       format: { with: HALF_WIDTH_NUMBER_REGEX, message: 'Half-width number' }
+    validates :spin_id,        numericality: { other_than: 1, message: 'Select' }
+    validates :speed_id,       numericality: { other_than: 1, message: 'Select' }
+    validates :price,          format: { with: HALF_WIDTH_NUMBER_REGEX, message: 'Half-width number' }
+    validates :level_id,       numericality: { other_than: 1, message: 'Select' }
+    validates :evaluation_id,  numericality: { other_than: 1, message: 'Select' }
+    validates :content
+  end
 end
