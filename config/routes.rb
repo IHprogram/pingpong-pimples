605ff1368224e9153ae9e12641ad82e0cfdd61d9
IHprogram <ihm20202020@gmail.com>
2020-09-17 17:37:28 +0900

    ルーティングの再設定
    searchアクションに関するルーティングを再設定した。

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: "reviews#index"
   resources :reviews do
     resources :comments, only: :create
+    collection do
+      get 'search'
+    end
   end
-  get 'reviews/search'
 end
