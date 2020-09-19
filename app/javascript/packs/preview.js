436ac60d754829bcb4416111308544018e75d1ff
IHprogram <ihm20202020@gmail.com>
2020-09-19 12:45:38 +0900

    値の変化が起こったときに呼び出される関数の定義
    値の変化、つまり画像が選択されたときに呼び出される関数を定義した。

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -2,5 +2,7 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
   document.addEventListener('DOMContentLoaded', function(){
     
     const ImageList = document.getElementById('image-list');
+    document.getElementById('review-image').addEventListener('change', function(e){
+    });
   });
 }
