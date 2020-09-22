3cf2943344e97da7deed1a82519df993ceefeec3
IHprogram <ihm20202020@gmail.com>
2020-09-22 19:49:08 +0900

    likesコントローラーの修正・変更
    以下の点について修正・変更を行った。
    
    - createメソッドとdestroyメソッドの定義を修正した。
    - それに伴い、不要なset_reviewsメソッドとset_likesメソッドを削除した。
    - いいねをクリックすると、レビュー詳細表示画面に遷移するようにした。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,23 +1,14 @@
 class LikesController < ApplicationController
-  before_action :set_reviews, only: [:create, :destroy]
-  before_action :set_likes, only: [:create, :destroy]
-
+  
   def create
-    @like = Like.create(user_id: current_user.id, review_id: params[:review_id])
+    @like = Like.new(user_id: current_user.id, review_id: params[:review_id])
+    @like.save
+    redirect_to("/reviews/#{params[:review_id]}")
   end
 
   def destroy
     @like = Like.find_by(user_id: current_user.id, review_id: params[:review_id])
     @like.destroy
-  end
-
-  private
-
-  def set_reviews
-    @reviews = Review.all
-  end
-
-  def set_likes
-    @likes = Like.where(review_id: params[:review_id])
+    redirect_to("/reviews/#{params[:review_id]}")
   end
 end
