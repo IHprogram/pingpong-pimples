473492cecffd729f68239326876b97df19d907dd
IHprogram <ihm20202020@gmail.com>
2020-09-22 11:02:11 +0900

    set_reviewsメソッドの定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,6 @@
 class LikesController < ApplicationController
+  before_action :set_reviews, only: [:create, :destroy]
+
   def create
     @like = Like.create(user_id: current_user.id, review_id: params[:review_id])
   end
@@ -6,5 +8,11 @@ class LikesController < ApplicationController
   def destroy
     like = Like.find_by(user_id: current_user.id, review_id: params[:review_id])
     like.destroy
+  end
+
+  private
+
+  def set_reviews
+    @reviews = Review.all
   end
 end
