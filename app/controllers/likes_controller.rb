414a34cb41975ec4f3ff829122ebb920516b0bdf
IHprogram <ihm20202020@gmail.com>
2020-09-22 10:49:47 +0900

    createメソッドの定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,7 @@
 class LikesController < ApplicationController
+  def create
+    @like = Like.create(user_id: current_user.id, review_id: params[:review_id])
+    @likes = Like.where(review_id: params[:review_id])
+    @reviews = Review.all
+  end
 end
