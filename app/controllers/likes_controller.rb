f6243be2d95fb284b77a940c4950d125fa4ca2bc
IHprogram <ihm20202020@gmail.com>
2020-09-22 11:03:04 +0900

    set_likesメソッドの定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,6 @@
 class LikesController < ApplicationController
   before_action :set_reviews, only: [:create, :destroy]
+  before_action :set_likes, only: [:create, :destroy]
 
   def create
     @like = Like.create(user_id: current_user.id, review_id: params[:review_id])
@@ -14,5 +15,9 @@ class LikesController < ApplicationController
 
   def set_reviews
     @reviews = Review.all
+  end
+
+  def set_likes
+    @likes = Like.where(review_id: params[:review_id])
   end
 end
