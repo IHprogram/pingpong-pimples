38714c367490add4eac9125bfdb4bf6766edb812
IHprogram <ihm20202020@gmail.com>
2020-09-23 18:38:34 +0900

    like.jsの読み込み
    この後に作成するlike.jsの読み込み。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 import '@fortawesome/fontawesome-free/js/all';
 require('./preview')
+require('./likes')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
