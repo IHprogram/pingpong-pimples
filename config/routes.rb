d0de8b46ed9a674b113c090a725b99ad7d581322
IHprogram <ihm20202020@gmail.com>
2020-09-23 10:08:14 +0900

    いいねした投稿一覧を表示するためのルーティング
    ユーザーがいいねした投稿を表示するためのルーティングを設定した。

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   end
   post "likes/:review_id/create" => "likes#create"
   post "likes/:review_id/destroy" => "likes#destroy"
+  get "users/:id/likes" => "users#likes"
   resources :users, only: :show
 end
