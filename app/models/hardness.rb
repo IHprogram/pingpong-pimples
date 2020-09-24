b1fc5d9d98618163325119d83328e0b7ae107c9e
IHprogram <ihm20202020@gmail.com>
2020-09-24 12:59:33 +0900

    Hardnessモデルの編集

--- app/models/hardness.rb
+++ app/models/hardness.rb
@@ -1,2 +1,10 @@
 class Hardness < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '軟らかい（ソフト）' },
+    { id: 3, name: 'やや軟らかい（セミソフト）' },
+    { id: 4, name: '平均的な硬さ（ミドル）' },
+    { id: 5, name: 'やや硬い（セミハード）' },
+    { id: 6, name: '硬い（ハード）' }
+  ]
 end
