f0418d7cbacbedec4ed6d676c98a5b31f4aa932f
IHprogram <ihm20202020@gmail.com>
2020-10-01 20:35:20 +0900

    createアクションにのみパスワードのバリデーションを適用
    プロフィール編集時に、パスワードのバリデーションが働かないようにするため。

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,5 @@ class User < ApplicationRecord
   validates :nickname, presence: true, uniqueness: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers', on: :create
 end
