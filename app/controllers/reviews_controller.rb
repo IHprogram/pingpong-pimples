d3034e4bab0f52314230c359e2924046e7189a25
IHprogram <ihm20202020@gmail.com>
2020-10-02 14:04:41 +0900

    現在ログインしているユーザーの情報の取得

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,6 +5,7 @@ class ReviewsController < ApplicationController
   before_action :search_review, only: [:index, :search]
 
   def index
+    @user = User.find(current_user.id)
     @reviews = Review.all.order('created_at DESC')
     set_review_column
   end
