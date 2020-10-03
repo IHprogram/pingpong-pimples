ca4c13b99912b8b74671bb887b36cfc94b15bcd3
IHprogram <ihm20202020@gmail.com>
2020-10-03 15:51:29 +0900

    記述した値の移動
    テストで使用する値を「user.rb」から「users.rb」に移動した。

--- spec/factories/user.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    nickname {Faker::Lorem.characters(number: 10)}
-    email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
-  end
-end
