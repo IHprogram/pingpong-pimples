594822a66a1069ee20825a25b48314acb9dd9785
IHprogram <ihm20202020@gmail.com>
2020-10-03 22:29:15 +0900

    Reviewモデルの単体テストで使用する値の作成
    テストで使用する値をFactoryBotで作成した。

---
+++ spec/factories/reviews.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :review do
+    name { Faker::Lorem.words }
+    manufacture { Manufacture.all.sample }
+    type { Type.all.sample }
+    hardness { Hardness.all.sample }
+    spin { Spin.all.sample }
+    speed { Speed.all.sample }
+    control { Control.all.sample }
+    price { Faker::Number.between(from: 1000, to: 20000) }
+    evaluation { Evaluation.all.sample}
+    content { Faker::Lorem.sentence }
+    association :user
+  end
+end
