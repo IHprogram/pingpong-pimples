3b26c688888141b707854b952338dec3afde613b
IHprogram <ihm20202020@gmail.com>
2020-10-04 17:20:19 +0900

    priceカラムに0円と入力された場合、レビューを投稿できないテスト

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -85,6 +85,12 @@ RSpec.describe Review, type: :model do
         @review.valid?
         expect(@review.errors.full_messages).to include("Price must be greater than 0")
       end
+
+      it 'priceは0円では投稿できないこと' do
+        @review.price = 0
+        @review.valid?
+        expect(@review.errors.full_messages).to include("Price must be greater than 0")
+      end
     end
   end
 end
