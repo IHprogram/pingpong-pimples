3dddcafb031894dbc7c16007010c1e0e5be99557
IHprogram <ihm20202020@gmail.com>
2020-10-06 15:06:21 +0900

    アソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :like_reviews, through: :likes, source: :review
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
   has_one_attached :image
 
   validates :nickname, presence: true, length: {maximum: 10}
