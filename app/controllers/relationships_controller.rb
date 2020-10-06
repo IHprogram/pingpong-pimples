48ae34d6b6399c890931d9816b178c1ef6993a99
IHprogram <ihm20202020@gmail.com>
2020-10-06 19:04:04 +0900

    destroyアクションの定義
    ユーザーのフォローを解除するdestroyアクションを設定した。

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -7,6 +7,12 @@ class RelationshipsController < ApplicationController
     redirect_to @user
   end
 
+  def destroy
+    following = current_user.unfollow(@user)
+    following.destroy
+    redirect_to @user
+  end
+
   private
 
   def set_user
