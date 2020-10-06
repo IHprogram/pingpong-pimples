f6a32b4445f43fe13848845eb2c40b786a8cca89
IHprogram <ihm20202020@gmail.com>
2020-10-06 23:10:49 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-  resources :relationships, only: [:create, :destroy]
+  post 'relationships/create', to: 'relationships#create'
+  delete 'relationships/destroy', to: 'relationships#destroy'
 end
