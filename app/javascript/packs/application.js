179e1fba58a7fd38c85c9b4a0ab40b321badc8eb
IHprogram <ihm20202020@gmail.com>
2020-10-08 12:43:59 +0900

    menu.jsの読み込み

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 import '@fortawesome/fontawesome-free/js/all';
 require('./preview')
 require('jquery')
+require('./menu')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
