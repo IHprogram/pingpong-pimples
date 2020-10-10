ab1dd10b1bc18d4fa06cad6f1016e717d0a1076c
IHprogram <ihm20202020@gmail.com>
2020-10-10 14:24:56 +0900

    フォロー数やフォロワー数の部分にマウスを重ねると背景色が変わる
    フォロー数やフォロワー数の部分にマウスを重ねると、背景色が黒色（#000000）からグレー（#696969）に変わる。マウスを離すと元の黒色（#000000）に戻る。

---
+++ app/javascript/packs/followings_followers.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const followingsNumber = document.getElementById('followings-number');
+  const followersNumber = document.getElementById('followers-number');
+  
+  followingsNumber.addEventListener('mouseover', () => {
+    followingsNumber.setAttribute('style', 'background-color: #696969;')
+  });
+  
+  followingsNumber.addEventListener('mouseout', () => {
+    followingsNumber.removeAttribute('style', 'background-color: #696969;')
+  });
+  
+  
+  followersNumber.addEventListener('mouseover', () => {
+    followersNumber.setAttribute('style', 'background-color: #696969;')
+  });
+  
+  followersNumber.addEventListener('mouseout', () => {
+    followersNumber.removeAttribute('style', 'background-color: #696969;')
+  });
+
+});
