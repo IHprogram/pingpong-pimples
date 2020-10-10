816f58cd3d08b5ff77a0b91201716fba51ac2b5b
IHprogram <ihm20202020@gmail.com>
2020-10-10 15:09:07 +0900

    フォロー一覧、フォロワー一覧の「閉じるボタン」をクリックした時の処理
    「閉じるボタン」をクリックすると、フォロー一覧・フォロワ一覧に「open-follow」というクラスを追加、または削除できる。（ここではクラスの削除目的で記述している）

--- app/javascript/packs/followings_followers.js
+++ app/javascript/packs/followings_followers.js
@@ -22,14 +22,24 @@ document.addEventListener('DOMContentLoaded', function() {
   // フォロー数、フォロワー数にマウスを乗せた場合、背景色が変わる
 
   // フォロー数、フォロワー数をクリックすると、フォロー一覧、フォロワー一覧が表示される
-    const followings =document.getElementById('followings-wrapper');
+    const followings = document.getElementById('followings-wrapper');
     const followers = document.getElementById('followers-wrapper');
+    const followingsCloseBtn = document.getElementById('followings-close-btn');
+    const followersCloseBtn = document.getElementById('followers-close-btn');
 
     followingsNumber.addEventListener('click', () => {
       followings.classList.toggle('open-follow')
     });
 
     followersNumber.addEventListener('click', () => {
+      followers.classList.toggle('open-follow')
+    });
+
+    followingsCloseBtn.addEventListener('click', () => {
+      followings.classList.toggle('open-follow')
+    });
+
+    followersCloseBtn.addEventListener('click', () => {
       followers.classList.toggle('open-follow')
     });
   // フォロー数、フォロワー数をクリックすると、フォロー一覧、フォロワー一覧が表示される
