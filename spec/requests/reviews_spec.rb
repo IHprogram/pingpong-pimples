a7956a4c2f6fd875d0cfc55a9da3b6dbb87306ef
IHprogram <ihm20202020@gmail.com>
2020-11-13 21:59:11 +0900

    indexアクションにおけるレスポンスのテスト

--- spec/requests/reviews_spec.rb
+++ spec/requests/reviews_spec.rb
@@ -3,8 +3,13 @@ describe ReviewsController, type: :request do
   before do
     @review = FactoryBot.build(:review)
     @review.image = fixture_file_upload('public/images/test.jpg')
+    @review.save
   end
 
   describe "GET #index" do
+    it "indexアクションにリクエストすると正常にレスポンスが返ってくる" do 
+      get root_path
+      expect(response.status).to eq 200
+    end
   end
 end
