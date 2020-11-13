54653a0291e735f360fcfae5eaea3618cb0ce7c9
IHprogram <ihm20202020@gmail.com>
2020-11-13 23:54:18 +0900

    reviewsコントローラーのindexアクションに関するテストを作成

--- spec/requests/reviews_spec.rb
+++ spec/requests/reviews_spec.rb
@@ -11,5 +11,25 @@ describe ReviewsController, type: :request do
       get root_path
       expect(response.status).to eq 200
     end
+    
+    it "indexアクションにリクエストするとレスポンスに投稿済みのレビューの商品名が表示されている" do 
+      get root_path
+      expect(response.body).to include @review.name
+    end
+    
+    it "indexアクションにリクエストするとレスポンスに投稿済みのレビューの投稿者名が表示されている" do
+      get root_path
+      expect(response.body).to include @review.user.nickname
+    end
+
+    it "indexアクションにリクエストするとレスポンスに投稿済みのレビューのメーカー名が表示されている" do
+      get root_path
+      expect(response.body).to include @review.manufacture.name
+    end
+
+    it "indexアクションにリクエストするとレスポンスに投稿済みのレビューの総合評価点数が表示されている" do
+      get root_path
+      expect(response.body).to include @review.evaluation.name
+    end
   end
 end
