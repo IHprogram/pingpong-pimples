5e93717402f4b9ad4a0064e21e169f03af7b8ed3
IHprogram <ihm20202020@gmail.com>
2020-11-13 21:57:34 +0900

    レビューのダミーデータを変更
    Active Hashを利用している7つのカラムに関しては、テストデータの値を「2」で固定するようにした。
    理由は、これらのカラムは「1（プルダウンの『--』）を選択すると、保存できない」というバリデーションを設定しており、いずれかのカラムの値が1の時はテストが失敗してしまうため。

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :review do
     name { Faker::Name.name }
-    manufacture { Manufacture.all.sample }
-    type { Type.all.sample }
-    hardness { Hardness.all.sample }
-    spin { Spin.all.sample }
-    speed { Speed.all.sample }
-    control { Control.all.sample }
+    manufacture_id { 2 }
+    type_id { 2 }
+    hardness_id { 2 }
+    spin_id { 2 }
+    speed_id { 2 }
+    control_id { 2 }
+    evaluation_id { 2 }
     price { Faker::Number.between(from: 1000, to: 20_000) }
-    evaluation { Evaluation.all.sample }
     content { Faker::Lorem.sentence }
     association :user
   end
