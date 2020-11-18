7b81d0a70d90eda0d96a9cc5133a2f95ced3c74e
IHprogram <ihm20202020@gmail.com>
2020-11-18 17:18:05 +0900

    reviewsコントローラーのshowアクションに関する単体テストを実装

--- spec/requests/reviews_spec.rb
+++ spec/requests/reviews_spec.rb
@@ -32,4 +32,61 @@ describe ReviewsController, type: :request do
       expect(response.body).to include @review.evaluation.name
     end
   end
+
+  describe 'GET #show' do
+    it "showアクションにリクエストすると正常にレスポンスが返ってくる" do 
+      get review_path(@review)
+      expect(response.status).to eq 200
+    end
+
+    it 'showアクションにリクエストするとレスポンスにレビューの投稿者名が表示されている' do
+      get review_path(@review)
+      expect(response.body).to include @review.user.nickname
+    end
+
+    it 'showアクションにリクエストするとレスポンスに商品名が表示されている' do
+      get review_path(@review)
+      expect(response.body).to include @review.manufacture.name
+    end
+
+    it 'showアクションにリクエストするとレスポンスにラバーの種類が表示されている' do
+      get review_path(@review)
+      expect(response.body).to include @review.type.name
+    end
+
+    it 'showアクションにリクエストするとレスポンスに打球感が表示されている' do
+      get review_path(@review)
+      expect(response.body).to include @review.hardness.name
+    end
+
+    it 'showアクションにリクエストするとレスポンスに価格が表示されている' do
+      get review_path(@review)
+      expect(response.body).to include @review.price.to_s
+    end
+
+    it 'showアクションにリクエストするとレスポンスにスピード性能の点数が表示されている' do
+      get review_path(@review)
+      expect(response.body).to include @review.speed.name
+    end
+
+    it 'showアクションにリクエストするとレスポンスにスピン性能の点数が表示されている' do
+      get review_path(@review)
+      expect(response.body).to include @review.spin.name
+    end
+
+    it 'showアクションにリクエストするとレスポンスにコントロール性能の点数が表示されている' do
+      get review_path(@review)
+      expect(response.body).to include @review.control.name
+    end
+
+    it 'showアクションにリクエストするとレスポンスに総合評価の点数が表示されている' do
+      get review_path(@review)
+      expect(response.body).to include @review.evaluation.name
+    end
+
+    it 'showアクションにリクエストするとレスポンスにその他コメントが表示されている' do
+      get review_path(@review)
+      expect(response.body).to include @review.content
+    end
+  end
 end
