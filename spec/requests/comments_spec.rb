715d2f87d5efbc1a2855e24f1c12845b9cc95d98
IHprogram <ihm20202020@gmail.com>
2020-11-25 20:01:49 +0900

    「GET #create」を「POST #create」に修正

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-  describe "GET #create" do
+  describe "POST #create" do
     let(:user) { FactoryBot.create(:user) }
     let(:review) {FactoryBot.create(:review, user: user)}
 
