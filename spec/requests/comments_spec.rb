eb72461cb6a33945239cf4a70c312817a33741dd
IHprogram <ihm20202020@gmail.com>
2020-11-26 12:48:02 +0900

    「投稿した本人であればコメントを削除できる」というテストを実装

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -20,6 +20,21 @@ RSpec.describe CommentsController, type: :controller do
         post :create, format: ["text/html"], params: { review_id: review.id, comment: comment_params }
       }.to_not change(user.comments, :count)
     end
+  end
 
+  describe "DELETE #destroy" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:review) {FactoryBot.create(:review, user: user)}
+    let!(:comment) {FactoryBot.create(:comment, review: review)}
+    
+    context "コメントを投稿したユーザー本人であれば" do
+
+      it "コメントを削除できること" do
+        sign_in user
+        expect {
+          delete :destroy, format: ["text/html"], params: { id: comment.id }
+        }.to change{ Comment.count }.by(-1)
+      end
+    end
   end
 end
