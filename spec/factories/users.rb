177ac14f56c168543623c30ac2e210f4e77d6a9d
IHprogram <ihm20202020@gmail.com>
2020-11-26 23:29:54 +0900

    ニックネームのテストデータの文字数を30文字に変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user, aliases: [:follow] do
-    nickname { Faker::Lorem.characters(number: 10) }
+    nickname { Faker::Lorem.characters(number: 30) }
     email { Faker::Internet.free_email }
     password = "a12345"
     password { password }
