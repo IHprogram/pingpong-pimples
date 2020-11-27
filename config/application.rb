e1675524a4b3ca10aeedc0987f23061c171409c0
IHprogram <ihm20202020@gmail.com>
2020-11-27 12:05:36 +0900

    不要なファイルを作成しない設定

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,12 @@ module PingpongPimples
     # the framework and any gems in your application.
     
     config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.test_framework :rspec,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
