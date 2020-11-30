1d2e5263046f31cc39a967c12f9bd7596ead036c
IHprogram <ihm20202020@gmail.com>
2020-11-30 23:12:02 +0900

    ログインが成功する場合の結合テストを実装

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,27 +2,59 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
 
-  let!(:user) { FactoryBot.build(:user) }
-  scenario "新規登録を行う" do
-    # トップページを開く
-    visit root_path
-    find('div[class="menu-wrapper"]').click
-    # メニューボタンをクリックすると、新規登録ボタンがある。
-    expect(page).to have_content('新規登録')
-    find('a[class="login"]').click
-    # 新規登録画面へ移動する。
-    visit new_user_registration_path
-    # ニックネームを入力
-    fill_in 'nickname', with: user.nickname
-    # メールアドレスを入力
-    fill_in 'メールアドレス', with: user.email
-    # パスワードを入力
-    fill_in 'パスワード', with: user.password
-    # 確認用パスワードを入力
-    fill_in 'パスワード(再入力)', with: user.password_confirmation
-    # 会員登録ボタンをクリックすると、ユーザーモデルのカウントが1上がる
-    expect{
-      find('input[name="commit"]').click
-    }.to change{ User.count }.by(1)
+  describe "新規登録を行う時" do
+    context "正しい値を入力すれば" do
+      let!(:user) { FactoryBot.build(:user) }
+      scenario "新規登録が成功すること" do
+        # トップページを開く
+        visit root_path
+        find('div[class="menu-wrapper"]').click
+        # メニューボタンをクリックすると、新規登録ボタンがある。
+        expect(page).to have_content('新規登録')
+        find('a[class="sign-up"]').click
+        # 新規登録画面へ移動する。
+        visit new_user_registration_path
+        # ニックネームを入力
+        fill_in 'nickname', with: user.nickname
+        # メールアドレスを入力
+        fill_in 'メールアドレス', with: user.email
+        # パスワードを入力
+        fill_in 'パスワード', with: user.password
+        # 確認用パスワードを入力
+        fill_in 'パスワード(再入力)', with: user.password_confirmation
+        # 会員登録ボタンをクリックすると、ユーザーモデルのカウントが1上がる
+        expect{
+          find('input[name="commit"]').click
+        }.to change{ User.count }.by(1)
+      end
+    end
+
+    context "誤った値を入力すれば" do
+      scenario "新規登録が失敗し、新規登録画面に戻ること" do
+      end
+    end
+  end
+
+  describe "ログインを行う時" do
+    context "正しい値を入力すれば" do
+      let!(:user) { FactoryBot.create(:user) }
+      scenario "ログインに成功し、トップページに移動すること" do
+        visit root_path
+        find('div[class="menu-wrapper"]').click
+        # メニューボタンをクリックすると、新規登録ボタンがある
+        expect(page).to have_content('ログイン')
+        find('a[class="login"]').click
+        # ログイン仮面へ移動する。
+        visit new_user_session_path
+        # メールアドレスを入力
+        fill_in 'メールアドレス', with: user.email
+        # パスワードを入力
+        fill_in 'パスワード', with: user.password
+        # ログインボタンをクリック
+        find('input[name="commit"]').click
+        # ログインに成功すれば、トップページに移動する
+        expect(current_path).to eq root_path
+      end
+    end
   end
 end
