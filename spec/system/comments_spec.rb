0f12a6711cff91b4e6fd84ebfd522707f18c82e4
IHprogram <ihm20202020@gmail.com>
2020-12-18 23:49:19 +0900

    コメントを正常に投稿できることをテスト

---
+++ spec/system/comments_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "Comments", type: :system do
+  describe "コメント投稿機能" do
+    let!(:review) { FactoryBot.create(:review) }
+    let!(:comment) { FactoryBot.build(:comment, review: review) }
+    before do
+      # トップページを開く
+      visit root_path
+      # ゲストログインボタンをクリック
+      find('a[class="guest-login-btn"]').click
+      # トップページ上部に「ログインしました」と表示される
+      expect(page).to have_content('ログインしました')
+      # レビューの詳細画面へ移動する。
+      visit review_path(review)
+    end
+
+    context 'ユーザーがコメント入力フォームに正常な値を入力すれば' do
+      it 'コメントを投稿できること' do
+        fill_in 'comment_content', with: comment.content
+        expect {
+          find('input[class="comment-btn"]').click
+          # ajaxが完了するまで待つ
+          wait_for_ajax do
+            expect(page).to have_content comment.content
+          end
+        }.to change { Comment.count }.by(1)
+      end
+    end
+  end
+end
