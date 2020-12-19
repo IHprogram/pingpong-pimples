0da50e49f7299f62946a4fc918bab26e0fda51ea
IHprogram <ihm20202020@gmail.com>
2020-12-19 23:55:38 +0900

    コメントの件数を表示する部分が正常に機能していることを確認

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -11,12 +11,15 @@ RSpec.describe "Comments", type: :system do
       find('a[class="guest-login-btn"]').click
       # トップページ上部に「ログインしました」と表示される
       expect(page).to have_content('ログインしました')
-      # レビューの詳細画面へ移動する。
+      # レビューの詳細画面へ移動する
       visit review_path(review)
     end
 
     context 'ユーザーがコメント入力フォームに正常な値を入力すれば' do
       it 'コメントを投稿できること' do
+        # まず、コメントの件数は0であること
+        expect(page).to have_content "0"
+        expect(page).to have_content "件のコメント"
         fill_in 'comment_content', with: comment.content
         expect {
           find('input[id="comment-button"]').click
@@ -26,6 +29,9 @@ RSpec.describe "Comments", type: :system do
             expect(page).to have_content comment.content
           end
         }.to change { Comment.count }.by(1)
+        # コメントの件数が1増えた状態で表示されていること
+        expect(page).to have_content "1"
+        expect(page).to have_content "件のコメント"
       end
     end
 
@@ -46,16 +52,20 @@ RSpec.describe "Comments", type: :system do
       find('a[class="guest-login-btn"]').click
       # トップページ上部に「ログインしました」と表示される
       expect(page).to have_content('ログインしました')
-      # レビューの詳細画面へ移動する。
+      # レビューの詳細画面へ移動する
       visit review_path(review)
     end
 
     it 'コメントを削除できること' do
       fill_in 'comment_content', with: comment.content
       find('input[id="comment-button"]').click
+      # コメントの件数は1件の状態から始まっていること
+      expect(page).to have_content "1"
+      expect(page).to have_content "件のコメント"
       find('a[class="comment-delete-btn"]').click
       expect {
         expect(page.accept_confirm).to eq "コメントを完全に削除してもよろしいですか？"
+        # コメントの件数が1件減って0件と表示されていること
         expect(page).to have_content "0"
         expect(page).to have_content "件のコメント"
       }.to change{ Comment.count }.by(-1)
