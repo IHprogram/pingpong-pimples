c84417f698ab890cc11fb4a3d1d95869680f564e
IHprogram <ihm20202020@gmail.com>
2020-12-19 15:11:00 +0900

    コメント削除機能の結合テスト

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -35,4 +35,30 @@ RSpec.describe "Comments", type: :system do
       end
     end
   end
+
+  describe "コメント削除機能" do
+    let!(:review) { FactoryBot.create(:review) }
+    let!(:comment) { FactoryBot.build(:comment, review: review) }
+    before do
+      # トップページを開く
+      visit root_path
+      # ゲストログインボタンをクリック
+      find('a[class="guest-login-btn"]').click
+      # トップページ上部に「ログインしました」と表示される
+      expect(page).to have_content('ログインしました')
+      # レビューの詳細画面へ移動する。
+      visit review_path(review)
+    end
+
+    it 'コメントを削除できること' do
+      fill_in 'comment_content', with: comment.content
+      find('input[id="comment-button"]').click
+      find('a[class="comment-delete-btn"]').click
+      expect {
+        expect(page.accept_confirm).to eq "コメントを完全に削除してもよろしいですか？"
+        expect(page).to have_content "0"
+        expect(page).to have_content "件のコメント"
+      }.to change{ Comment.count }.by(-1)
+    end
+  end
 end
