a1b5c7f2cd57d58c6816516dc49d4d3c69640a92
IHprogram <ihm20202020@gmail.com>
2020-12-19 11:42:52 +0900

    コメント送信ボタンの表示に関するJavaScriptやCSSの実装

---
+++ app/javascript/packs/comment_button.js
@@ -0,0 +1,28 @@
+if (document.URL.match( /reviews/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const commentForm = document.getElementById('comment_content');
+    const commentBtn = document.getElementById('comment-button');
+    
+    console.log(commentForm);
+    
+    commentForm.addEventListener('change', function() {
+      let commentContent = commentForm.value;
+      if (commentContent == "" ) {
+        commentBtn.classList.remove('open-btn');
+        commentBtn.classList.add('close-btn');
+      }else{
+        commentBtn.classList.remove('close-btn');
+        commentBtn.classList.add('open-btn');
+      };
+    });
+    
+
+    commentBtn.addEventListener('click', function() {
+      let commentContent = commentForm.value;
+      if (commentContent == "" ) {
+        commentBtn.classList.remove('open-btn');
+        commentBtn.classList.add('close-btn');
+      };
+    });
+  });
+}
