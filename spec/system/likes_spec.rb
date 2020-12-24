51b8e89e61a33a5b787373a4855d3e32f71f391f
IHprogram <ihm20202020@gmail.com>
2020-12-24 12:06:30 +0900

    レビューの詳細画面に「いいね！」ボタンが表示されていることをテスト

--- spec/system/likes_spec.rb
+++ spec/system/likes_spec.rb
@@ -2,10 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "Likes", type: :system do
   describe 'いいね機能' do
-    let!(:user) { FactoryBot.create(:user) }
-    let!(:review) { FactoryBot.create(:review, user: user) }    
+    context 'ユーザーがログインしている時' do
+      let!(:user) { FactoryBot.create(:user) }
+      let!(:review) { FactoryBot.create(:review, user: user) }
+      before do
+        # トップページを開く
+        visit root_path
+        # ゲストログインボタンをクリック
+        find('a[class="guest-login-btn"]').click
+        # トップページ上部に「ログインしました」と表示される
+        expect(page).to have_content('ログインしました')
+        # レビューの詳細画面へ移動する
+        visit review_path(review)
+      end
+      
+      it 'レビューの詳細画面に「いいね！」ボタンが表示されていること' do
+        expect(page).to have_content('いいね！')
+      end
+    end
 
     context 'ユーザーがログインしていない時' do
+      let!(:user) { FactoryBot.create(:user) }
+      let!(:review) { FactoryBot.create(:review, user: user) }
       before do
         # トップページを開く
         visit root_path
